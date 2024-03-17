c6ffd6dccc5c7c951058b6b124c45de3d7fbd7c1
coolguy284 <coolguy284v64@gmail.com>
2024-03-17 00:28:27 -0400

    add julian date class
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- py_time_lib/tests/test_calendar_julian.py
+++ py_time_lib/tests/test_calendar_julian.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ..calendars.julian import days_in_year, is_leap
+from ..calendars.julian import days_in_year, is_leap, JulianDate
 
 class TestCalendarJulian(unittest.TestCase):
   def test_leap_year_2020(self):
@@ -23,3 +23,11 @@ class TestCalendarJulian(unittest.TestCase):
   
   def test_year_1900_days(self):
     self.assertEqual(days_in_year(1900), 366)
+  
+  def test_repr(self):
+    jd = JulianDate(2024, 1, 1)
+    self.assertEqual(repr(jd), 'JulianDate(2024, 1, 1)')
+  
+  def test_str(self):
+    jd = JulianDate(2024, 1, 1)
+    self.assertEqual(str(jd), '2024-01-01')
