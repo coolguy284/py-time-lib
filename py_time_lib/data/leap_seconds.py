4e651ec8afef6dcb31d368249987b62a6948d552
coolguy284 <coolguy284v64@gmail.com>
2024-03-28 18:59:37 -0400

    move leap seconds list to data file

---
+++ py_time_lib/data/leap_seconds.py
@@ -0,0 +1,33 @@
+from ..fixed_prec import FixedPrec
+
+# data from https://www.nist.gov/pml/time-and-frequency-division/time-realization/leap-seconds
+LEAP_SECONDS = [
+  # [date, utc delta]
+  ['1972-06-30', FixedPrec(-1, 0)],
+  ['1972-12-31', FixedPrec(-1, 0)],
+  ['1973-12-31', FixedPrec(-1, 0)],
+  ['1974-12-31', FixedPrec(-1, 0)],
+  ['1975-12-31', FixedPrec(-1, 0)],
+  ['1976-12-31', FixedPrec(-1, 0)],
+  ['1977-12-31', FixedPrec(-1, 0)],
+  ['1978-12-31', FixedPrec(-1, 0)],
+  ['1979-12-31', FixedPrec(-1, 0)],
+  ['1981-06-30', FixedPrec(-1, 0)],
+  ['1982-06-30', FixedPrec(-1, 0)],
+  ['1983-06-30', FixedPrec(-1, 0)],
+  ['1985-06-30', FixedPrec(-1, 0)],
+  ['1987-12-31', FixedPrec(-1, 0)],
+  ['1989-12-31', FixedPrec(-1, 0)],
+  ['1990-12-31', FixedPrec(-1, 0)],
+  ['1992-06-30', FixedPrec(-1, 0)],
+  ['1993-06-30', FixedPrec(-1, 0)],
+  ['1994-06-30', FixedPrec(-1, 0)],
+  ['1995-12-31', FixedPrec(-1, 0)],
+  ['1997-06-30', FixedPrec(-1, 0)],
+  ['1998-12-31', FixedPrec(-1, 0)],
+  ['2005-12-31', FixedPrec(-1, 0)],
+  ['2008-12-31', FixedPrec(-1, 0)],
+  ['2012-06-30', FixedPrec(-1, 0)],
+  ['2015-06-30', FixedPrec(-1, 0)],
+  ['2016-12-31', FixedPrec(-1, 0)],
+]
--- py_time_lib/time_classes.py
+++ py_time_lib/time_classes.py
@@ -1,5 +1,6 @@
 from .fixed_prec import FixedPrec
-from .calendars.gregorian import GregorianDate
+#from .calendars.gregorian import GregorianDate
+from .data.leap_seconds import LEAP_SECONDS as DATA_LEAP_SECONDS
 
 class TimeDelta:
   __slots__ = '_time_delta'
@@ -57,36 +58,7 @@ class TimeInstant:
   # leap second functionality unimplemented for now
   # data from https://www.nist.gov/pml/time-and-frequency-division/time-realization/leap-seconds
   UTC_INITIAL_OFFSET_FROM_TAI = FixedPrec(-10, 0)
-  LEAP_SECONDS = [
-    # [date, utc delta]
-    ['1972-06-30', FixedPrec(-1, 0)],
-    ['1972-12-31', FixedPrec(-1, 0)],
-    ['1973-12-31', FixedPrec(-1, 0)],
-    ['1974-12-31', FixedPrec(-1, 0)],
-    ['1975-12-31', FixedPrec(-1, 0)],
-    ['1976-12-31', FixedPrec(-1, 0)],
-    ['1977-12-31', FixedPrec(-1, 0)],
-    ['1978-12-31', FixedPrec(-1, 0)],
-    ['1979-12-31', FixedPrec(-1, 0)],
-    ['1981-06-30', FixedPrec(-1, 0)],
-    ['1982-06-30', FixedPrec(-1, 0)],
-    ['1983-06-30', FixedPrec(-1, 0)],
-    ['1985-06-30', FixedPrec(-1, 0)],
-    ['1987-12-31', FixedPrec(-1, 0)],
-    ['1989-12-31', FixedPrec(-1, 0)],
-    ['1990-12-31', FixedPrec(-1, 0)],
-    ['1992-06-30', FixedPrec(-1, 0)],
-    ['1993-06-30', FixedPrec(-1, 0)],
-    ['1994-06-30', FixedPrec(-1, 0)],
-    ['1995-12-31', FixedPrec(-1, 0)],
-    ['1997-06-30', FixedPrec(-1, 0)],
-    ['1998-12-31', FixedPrec(-1, 0)],
-    ['2005-12-31', FixedPrec(-1, 0)],
-    ['2008-12-31', FixedPrec(-1, 0)],
-    ['2012-06-30', FixedPrec(-1, 0)],
-    ['2015-06-30', FixedPrec(-1, 0)],
-    ['2016-12-31', FixedPrec(-1, 0)],
-  ]
+  LEAP_SECONDS = DATA_LEAP_SECONDS
   
   @classmethod
   def _init_class_vars(cls):
