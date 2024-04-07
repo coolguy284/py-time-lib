bdcceeb6a176e653154064deccfb0d0423dd6387
coolguy284 <coolguy284v64@gmail.com>
2024-04-07 01:38:53 -0400

    test holocene and isoweek repr and str
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- py_time_lib/tests/test_calendar_holocene.py
+++ py_time_lib/tests/test_calendar_holocene.py
@@ -6,3 +6,9 @@ from ..calendars.gregorian import GregorianDate
 class TestCalendarHolocene(unittest.TestCase):
   def test_basic_creation(self):
     self.assertEqual(HoloceneDate(GregorianDate(2024, 4, 7)).to_date_tuple(), (12024, 4, 7))
+  
+  def test_repr(self):
+    self.assertEqual(repr(HoloceneDate(12024, 2, 3)), 'HoloceneDate(year = 12024, month = 2, day = 3)')
+  
+  def test_str(self):
+    self.assertEqual(str(HoloceneDate(12024, 2, 3)), '12024-02-03')
--- py_time_lib/tests/test_calendar_isoweek.py
+++ py_time_lib/tests/test_calendar_isoweek.py
@@ -5,3 +5,9 @@ from ..calendars.iso_weekdate import IsoWeekDate
 class TestCalendarIsoWeek(unittest.TestCase):
   def test_instantiate(self):
     IsoWeekDate(0)
+  
+  def test_repr(self):
+    self.assertEqual(repr(IsoWeekDate(2024, 2, 3)), 'IsoWeekDate(year = 2024, week = 2, day = 3)')
+  
+  def test_str(self):
+    self.assertEqual(str(IsoWeekDate(2024, 2, 3)), '2024-W02-3')
