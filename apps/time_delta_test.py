847702f4b00ae9960a1d24e1a1c92040323a4bba
coolguy284 <coolguy284v64@gmail.com>
2024-04-27 00:19:09 -0400

    create time delta tester app
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

---
+++ apps/time_delta_test.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from time import sleep, perf_counter_ns, monotonic_ns, time_ns
+import os, sys
+
+# https://stackoverflow.com/questions/60593604/importerror-attempted-relative-import-with-no-known-parent-package
+# https://stackoverflow.com/questions/5137497/find-the-current-directory-and-files-directory
+parent_dir = Path(os.path.realpath(__file__)).parent.parent
+sys.path.append(str(parent_dir))
+
+from py_time_lib import FixedPrec
+from py_time_lib.constants import NOMINAL_NANOSECS_PER_SEC_LOG_FIXEDPREC_RADIX
+
+def format_offsets(delta_time, delta_monotone):
+  return (
+    f'{f'{FixedPrec(delta_time, NOMINAL_NANOSECS_PER_SEC_LOG_FIXEDPREC_RADIX)!s}':>12}  ' +
+    f'{f'{FixedPrec(delta_monotone, NOMINAL_NANOSECS_PER_SEC_LOG_FIXEDPREC_RADIX)!s}':>12}'
+  )
+
+def times():
+  delta_time = time_ns() - perf_counter_ns()
+  delta_monotone = monotonic_ns() - perf_counter_ns()
+  return delta_time, delta_monotone
+
+delta_time_initial, delta_monotone_initial = times()
+
+def times_delta():
+  delta_time, delta_monotone = times()
+  return format_offsets(delta_time - delta_time_initial, delta_monotone - delta_monotone_initial)
+
+print('Initial offsets:')
+print(format_offsets(delta_time_initial, delta_monotone_initial))
+
+while True:
+  print(times_delta())
+  sleep(1)
