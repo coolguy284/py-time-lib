78b4bdfbf83449517a08e6732a9e862dff99174d
coolguy284 <coolguy284v64@gmail.com>
2024-04-28 09:36:48 -0400

    gui time display: clean up font gen
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- apps/gui_time_display.py
+++ apps/gui_time_display.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-import os, pygame, sys
+import os, sys
 
 # https://stackoverflow.com/questions/60593604/importerror-attempted-relative-import-with-no-known-parent-package
 # https://stackoverflow.com/questions/5137497/find-the-current-directory-and-files-directory
 parent_dir = Path(os.path.realpath(__file__)).parent.parent
 sys.path.append(str(parent_dir))
 
+from functools import cache
+import pygame
 from py_time_lib import TimeInstant
 
 pygame.init()
@@ -20,8 +22,20 @@ clock = pygame.time.Clock()
 
 consolas = pygame.font.SysFont('Consolas', 35)
 
-def draw_centered(surf: pygame.Surface, font: pygame.font.Font, text: str, pos: tuple[int, int]) -> None:
-  font_rendered = font.render(text, True, (255, 255, 255))
+@cache
+def get_font(font: str, size: int):
+  return pygame.font.SysFont(font, size)
+
+def draw_text_centered(
+    surf: pygame.Surface,
+    text: str,
+    pos: tuple[int, int],
+    font: str = 'Consolas',
+    size: int = 35,
+    color: tuple[int, int, int] = (255, 255, 255)
+  ) -> None:
+  font = get_font(font, size)
+  font_rendered = font.render(text, True, color)
   coords = (
     pos[0] - font_rendered.get_width() / 2,
     pos[1] - font_rendered.get_height() / 2
@@ -39,9 +53,9 @@ while loop:
   
   now = TimeInstant.now()
   
-  draw_centered(screen, consolas, 'Current Time', (width / 2, 50))
-  draw_centered(screen, consolas, f'TAI: {now.to_format_string_tai(format_str)}', (width / 2, 120))
-  draw_centered(screen, consolas, f'UTC: {now.to_format_string_utc(format_str)}', (width / 2, 190))
+  draw_text_centered(screen, 'Current Time', (width / 2, 50), size = 43)
+  draw_text_centered(screen, f'TAI: {now.to_format_string_tai(format_str)}', (width / 2, 130))
+  draw_text_centered(screen, f'UTC: {now.to_format_string_utc(format_str)}', (width / 2, 200))
   
   pygame.display.flip()
   clock.tick(refresh_rate)
