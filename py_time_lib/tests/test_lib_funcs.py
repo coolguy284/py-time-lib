884d49fec03c8aa22ddc272da90ae4b57f85d32c
coolguy284 <coolguy284v64@gmail.com>
2024-04-30 03:15:22 -0400

    add tests for binary_search_float
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- py_time_lib/tests/test_lib_funcs.py
+++ py_time_lib/tests/test_lib_funcs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from .. import binary_search, binary_search_array_split
+from .. import binary_search, binary_search_array_split, binary_search_float, FixedPrec
 
 class TestLibFuncs(unittest.TestCase):
   def test_binary_search(self):
@@ -16,3 +16,7 @@ class TestLibFuncs(unittest.TestCase):
     self.assertEqual(binary_search_array_split(array, lambda x: x <= 0), ([0], [1, 8, 13, 26, 33, 44, 55, 66, 77, 145, 1001]))
     self.assertEqual(binary_search_array_split(array, lambda x: x <= 44), ([0, 1, 8, 13, 26, 33, 44], [55, 66, 77, 145, 1001]))
     self.assertEqual(binary_search_array_split(array, lambda x: x <= 1001), ([0, 1, 8, 13, 26, 33, 44, 55, 66, 77, 145, 1001], []))
+  
+  def test_binary_search_float(self):
+    self.assertEqual(binary_search_float(lambda x: x <= 3.14159, 3, 4), 3.14159)
+    self.assertEqual(binary_search_float(lambda x: x <= FixedPrec('18237645172386537123.141591236471'), FixedPrec(0), 10 ** 30), FixedPrec('18237645172386537123.141591236471'))
