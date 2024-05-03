e3eede1f8d7e82e7764ed018a9f26be707f4df83
coolguy284 <coolguy284v64@gmail.com>
2024-05-03 02:34:58 -0400

    ut1: add update routine to update dbs
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- py_time_lib/update_dbs.py
+++ py_time_lib/update_dbs.py
@@ -13,6 +13,19 @@ from .update_timezone_db import DEFAULT_TZDB_VERSION_URL as _DEFAULT_TZDB_VERSIO
 from .update_timezone_db import DEFAULT_TZDB_PATH as _DEFAULT_TZDB_PATH
 from .update_timezone_db import DEFAULT_TZDB_DOWNLOADED_TIME_PATH as _DEFAULT_TZDB_DOWNLOADED_TIME_PATH
 from .update_timezone_db import get_tzdb_data as _Tzdb_get_tzdb_data
+from .update_ut1 import DEFAULT_HISTORICAL_UPDATE_TIME as _DEFAULT_HISTORICAL_UPDATE_TIME
+from .update_ut1 import DEFAULT_HISTORICAL_DOWNLOADED_TIME_FILE_PATH as _DEFAULT_HISTORICAL_DOWNLOADED_TIME_FILE_PATH
+from .update_ut1 import DEFAULT_HISTORICAL_FILE_PATH as _DEFAULT_HISTORICAL_FILE_PATH
+from .update_ut1 import DEFAULT_HISTORICAL_URL as _DEFAULT_HISTORICAL_URL
+from .update_ut1 import DEFAULT_RECENT_UPDATE_TIME as _DEFAULT_RECENT_UPDATE_TIME
+from .update_ut1 import DEFAULT_RECENT_DOWNLOADED_TIME_FILE_PATH as _DEFAULT_RECENT_DOWNLOADED_TIME_FILE_PATH
+from .update_ut1 import DEFAULT_RECENT_FILE_PATH as _DEFAULT_RECENT_FILE_PATH
+from .update_ut1 import DEFAULT_RECENT_URL as _DEFAULT_RECENT_URL
+from .update_ut1 import DEFAULT_DAILY_UPDATE_TIME as _DEFAULT_DAILY_UPDATE_TIME
+from .update_ut1 import DEFAULT_DAILY_DOWNLOADED_TIME_FILE_PATH as _DEFAULT_DAILY_DOWNLOADED_TIME_FILE_PATH
+from .update_ut1 import DEFAULT_DAILY_FILE_PATH as _DEFAULT_DAILY_FILE_PATH
+from .update_ut1 import DEFAULT_DAILY_URL as _DEFAULT_DAILY_URL
+from .update_ut1 import get_ut1_offsets as _Ut1_get_ut1_offsets
 
 TIMEZONES: dict[str, dict[str, TimeZone]] = {
   'proleptic_variable': {},
@@ -48,6 +61,35 @@ def update_timezone_data(
   for key in new_data:
     TIMEZONES[key] = new_data[key]
 
+def update_ut1_offsets(
+    historic_min_redownload_age: TimeStorageType | None = _DEFAULT_HISTORICAL_UPDATE_TIME,
+    historic_downloaded_time_file_path: str = _DEFAULT_HISTORICAL_DOWNLOADED_TIME_FILE_PATH,
+    historic_data_file_path: str = _DEFAULT_HISTORICAL_FILE_PATH,
+    historic_url: str = _DEFAULT_HISTORICAL_URL,
+    recent_min_redownload_age: TimeStorageType | None = _DEFAULT_RECENT_UPDATE_TIME,
+    recent_downloaded_time_file_path: str = _DEFAULT_RECENT_DOWNLOADED_TIME_FILE_PATH,
+    recent_data_file_path: str = _DEFAULT_RECENT_FILE_PATH,
+    recent_url: str = _DEFAULT_RECENT_URL,
+    daily_min_redownload_age: TimeStorageType | None = _DEFAULT_DAILY_UPDATE_TIME,
+    daily_downloaded_time_file_path: str = _DEFAULT_DAILY_DOWNLOADED_TIME_FILE_PATH,
+    daily_data_file_path: str = _DEFAULT_DAILY_FILE_PATH,
+    daily_url: str = _DEFAULT_DAILY_URL
+  ) -> None:
+  TimeInstant.UT1_OFFSETS[:] = _Ut1_get_ut1_offsets(
+    historic_min_redownload_age,
+    historic_downloaded_time_file_path,
+    historic_data_file_path,
+    historic_url,
+    recent_min_redownload_age,
+    recent_downloaded_time_file_path,
+    recent_data_file_path,
+    recent_url,
+    daily_min_redownload_age,
+    daily_downloaded_time_file_path,
+    daily_data_file_path,
+    daily_url
+  )
+
 def update_time_databases(
     log_downloads: bool = _DEFAULT_LOG_DOWNLOADS,
     
@@ -58,7 +100,20 @@ def update_time_databases(
     tzdb_url: str = _DEFAULT_TZDB_URL,
     tzdb_version_url: str = _DEFAULT_TZDB_VERSION_URL,
     tzdb_file_path: str = _DEFAULT_TZDB_PATH,
-    tzdb_downloaded_time_file_path: str = _DEFAULT_TZDB_DOWNLOADED_TIME_PATH
+    tzdb_downloaded_time_file_path: str = _DEFAULT_TZDB_DOWNLOADED_TIME_PATH,
+    
+    ut1_historic_min_redownload_age: TimeStorageType | None = _DEFAULT_HISTORICAL_UPDATE_TIME,
+    ut1_historic_downloaded_time_file_path: str = _DEFAULT_HISTORICAL_DOWNLOADED_TIME_FILE_PATH,
+    ut1_historic_data_file_path: str = _DEFAULT_HISTORICAL_FILE_PATH,
+    ut1_historic_url: str = _DEFAULT_HISTORICAL_URL,
+    ut1_recent_min_redownload_age: TimeStorageType | None = _DEFAULT_RECENT_UPDATE_TIME,
+    ut1_recent_downloaded_time_file_path: str = _DEFAULT_RECENT_DOWNLOADED_TIME_FILE_PATH,
+    ut1_recent_data_file_path: str = _DEFAULT_RECENT_FILE_PATH,
+    ut1_recent_url: str = _DEFAULT_RECENT_URL,
+    ut1_daily_min_redownload_age: TimeStorageType | None = _DEFAULT_DAILY_UPDATE_TIME,
+    ut1_daily_downloaded_time_file_path: str = _DEFAULT_DAILY_DOWNLOADED_TIME_FILE_PATH,
+    ut1_daily_data_file_path: str = _DEFAULT_DAILY_FILE_PATH,
+    ut1_daily_url: str = _DEFAULT_DAILY_URL
   ) -> None:
   update_leap_seconds(
     log_downloads = log_downloads,
@@ -72,4 +127,18 @@ def update_time_databases(
     version_url = tzdb_version_url,
     db_file_path = tzdb_file_path,
     downloaded_time_file_path = tzdb_downloaded_time_file_path
+  )
+  update_ut1_offsets(
+    historic_min_redownload_age = ut1_historic_min_redownload_age,
+    historic_downloaded_time_file_path = ut1_historic_downloaded_time_file_path,
+    historic_data_file_path = ut1_historic_data_file_path,
+    historic_url = ut1_historic_url,
+    recent_min_redownload_age = ut1_recent_min_redownload_age,
+    recent_downloaded_time_file_path = ut1_recent_downloaded_time_file_path,
+    recent_data_file_path = ut1_recent_data_file_path,
+    recent_url = ut1_recent_url,
+    daily_min_redownload_age = ut1_daily_min_redownload_age,
+    daily_downloaded_time_file_path = ut1_daily_downloaded_time_file_path,
+    daily_data_file_path = ut1_daily_data_file_path,
+    daily_url = ut1_daily_url
   )
