b27cb3aac85dd72cd2b15be7f32460b54847d89b
coolguy284 <coolguy284v64@gmail.com>
2024-07-03 10:29:49 -0400

    update holidays list
    details: add memorial day and independence day to holidays
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- py_time_lib/data_py/holidays.py
+++ py_time_lib/data_py/holidays.py
@@ -5,7 +5,9 @@ from ..calendars.gregorian import GregorianDate
 
 HOLIDAYS = Enum('HOLIDAYS', (
   'MOTHERS_DAY',
+  'MEMORIAL_DAY',
   'FATHERS_DAY',
+  'AMERICAN_INDEPENDENCE_DAY',
 ))
 
 def get_holiday(holiday: HOLIDAYS, year: Integral) -> GregorianDate:
@@ -15,10 +17,19 @@ def get_holiday(holiday: HOLIDAYS, year: Integral) -> GregorianDate:
       # second sunday in may
       return GregorianDate.from_month_week_day(year, 5, 2, 0)
     
+    case HOLIDAYS.MEMORIAL_DAY:
+      # https://en.wikipedia.org/wiki/Memorial_Day
+      # last monday in may
+      return GregorianDate.from_month_week_day(year, 5, 1, 1, True)
+    
     case HOLIDAYS.FATHERS_DAY:
       # https://en.wikipedia.org/wiki/Father%27s_Day_(United_States)
       # third sunday in june
       return GregorianDate.from_month_week_day(year, 6, 3, 0)
+    
+    case HOLIDAYS.AMERICAN_INDEPENDENCE_DAY:
+      # fourth of july
+      return GregorianDate(year, 7, 4)
 
 def get_holidays(year: Integral) -> list[tuple[str, GregorianDate]]:
   holidays = []
